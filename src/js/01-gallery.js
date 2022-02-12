f2405f9a498e289ad53a889bb653211cf01bcbc3
skyandy1891 <skyandy1891@gmail.com>
2022-02-12 18:02:59 +0200

    add galery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(`div`);
+
+const newLinkImgEl = galleryItems.map((item) => `<a class="gallery__link" href="${item.original}" target="_self">
+
+<img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a>`).join("")
+
+galleryEl.insertAdjacentHTML(`beforeend`, newLinkImgEl)
