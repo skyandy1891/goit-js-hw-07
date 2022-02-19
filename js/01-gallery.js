83f436b2c191abd24312336f2626665ce9b4e585
skyandy1891 <skyandy1891@gmail.com>
2022-02-19 15:58:59 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-
+// =========================================================================
 const galleryEl = document.querySelector(`div`);
 
 const newLinkImgEl = galleryItems.map((item) => `<a class="gallery__link" href="${item.original}" target="_self">
@@ -11,3 +11,4 @@ const newLinkImgEl = galleryItems.map((item) => `<a class="gallery__link" href="
 <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a>`).join("")
 
 galleryEl.insertAdjacentHTML(`beforeend`, newLinkImgEl)
+// ======================================================================
